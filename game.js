b36e9ff98267e4af1ff51cb6ba6258f3122684e1
Matt Meleen <mmeleen@utexas.edu>
2020-06-22 02:03:00 -0500

    Functions w/ pseudo-code; sample question array

---
+++ game.js
@@ -0,0 +1,31 @@
+function startQuiz() {
+	// start timer function (need to make this)
+  // set text content of timer on DOM
+	// call getQuestion function
+}
+
+function getQuestion() {
+	// get the question/answer object from your questions array based on the current question index
+	// update the DOM with the current question
+  // clear out any old question choices  
+  // update the DOM with the current answer choices
+			// for loop that goes through the answer choices
+				// creates a button
+				// set a class attribute of "choice"
+				// set a value attribute of choice
+				// set text content of button to be choice
+				// append button to the DOM
+}
+
+function checkAnswer() {
+	// if choice value is incorrect
+		// subtract seconds from the timer
+		// display new time to page
+		// let player know they are incorrect
+	// else 
+		// let the player know they're correct
+
+	// check if we've run out of questions
+		// yes - end game
+		// no - get next question
+}
---
+++ questions.js
@@ -0,0 +1,17 @@
+var questionArray = [
+	{
+		title: "This is the first question...?",
+		choices: ["1 YES", "2 no", "3 no"],
+		answer: "1 YES"
+	},
+	{
+		title: "This is the second question...?",
+		choices: ["1 no", "2 YES", "3 no"],
+		answer: "2 YES"
+	},
+	{
+		title: "This is the third question...?",
+		choices: ["1 no", "2 no", "3 YES"],
+		answer: "3 YES"
+	}
+]
