8b5d46e5d39be9d69a79f422b0cf22c85cab1879
Matt Meleen <mmeleen@utexas.edu>
2020-06-25 18:12:06 -0500

    Updated question list

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-var questionArray = [
+/*var questionArrayTest = [
 	{
 		title: "This is the first question...?",
 		choices: ["1 YES", "2 no", "3 no"],
@@ -13,5 +13,58 @@ var questionArray = [
 		title: "This is the third question...?",
 		choices: ["1 no", "2 no", "3 YES"],
 		answer: "3 YES"
+	}
+]*/
+
+var questionArray = [	
+	{
+		title: "Inside which HTML element do we put the JavaScript?",
+		choices: ["A. <js>", "B. <script>", "C. <scripting>", "D. <javascript>"],
+		answer: "B. <script>"
+	},
+	{
+		title: "Where is the correct place to insert a JavaScript?",
+		choices: ["A. The <head> section", "B. The <body> section", "C. Either the <head> or <body>", "D. Outside of the <head> or <body>, but within the <html> tags"],
+		answer: "C. Either the <head> or <body>"
+	},
+	{
+		title: "How do you create a function in JavaScript?",
+		choices: ["A. function myFunction()", "B. function:myFunction()", "C. function = myFunction()", "D. fun myFunction()"],
+		answer: "A. function myFunction()"
+	},
+	{
+		title: "How do you declare a JavaScript variable?",
+		choices: ["A. var carName", "B. v carName", "C. variable carName", "D. var: carName"],
+		answer: "C. var carName"
+	},
+	{
+		title: "How do you write an IF statement in JavaScript?",
+		choices: ["A. if i = 5 then", "B. if (i == 5)", "C. if i == 5 then", "D. if i = 5"],
+		answer: "B. if (i == 5)"
+	},
+	{
+		title: "How does a WHILE loop start?",
+		choices: ["A. while (i <= 10)", "B. while i = 1 to 10", "C. while (i <= 10; i++)", "D. while i = 1, i <= 10"],
+		answer: "A. while (i <= 10)"
+	},
+	{
+		title: "How do you round the number 7.25, to the nearest integer?",
+		choices: ["A. Math.rnd(7.25)", "B. round(7.25)", "C. Math.round(7.25)", "D. rnd(7.25)"],
+		answer: "C. Math.round(7.25)"
+	},
+	{
+		title: "How do you find the largest of the numbers (x, y, z)?",
+		choices: ["A. Math.ceil(x,y)", "B. top(x,y,z)", "C. ceil(x,y,z)", "D. Math.max(x,y,z)"],
+		answer: "D. Math.max(x,y,z)"
+	},
+	{
+		title: "How can you detect the client's browser name?",
+		choices: ["A. client.navName", "B. navigator.appName", "C. browser.name", "D. window.browser"],
+		answer: "B. navigator.appName"
+	},
+	{
+		title: "Which event occurs when the user clicks on an HTML element?",
+		choices: ["A. onmouseclick", "B. onchange", "C. onmouseover", "D. onclick"],
+		answer: "D. onclick"
 	}
 ]
