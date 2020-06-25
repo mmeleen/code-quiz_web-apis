70196199b3ead644ccac6eff4e44aac76fcd1c7e
Matt Meleen <mmeleen@utexas.edu>
2020-06-25 18:52:40 -0500

    Improved comment documentation

--- score.js
+++ score.js
@@ -1,14 +1,16 @@
+// Retrieve and log stored high scores
 var retrievedHighscores = JSON.parse(localStorage.getItem("highScores"));
-
 console.log(retrievedHighscores);
 
+// Variable declaration
 var clearBtn = document.getElementById("clear-btn");
 
-
+// Sort high scores
 retrievedHighscores.sort(function(a,b){
   return b.score - a.score;
 })
 
+// Generate high scores list and add to page
 for(var i = 0; i < retrievedHighscores.length; i++){
   var highscoreDiv = document.createElement("li");
   highscoreDiv.classList.add("list-group-item")
@@ -17,6 +19,7 @@ for(var i = 0; i < retrievedHighscores.length; i++){
   document.getElementById("highscore-container").append(highscoreDiv);
 }
 
+// Clear high scores
 function clearScores() {
 	console.log(event.target);
   localStorage.clear();
@@ -25,4 +28,5 @@ function clearScores() {
   highScores = [];
 }
 
+// Event listener for Clear Scores button
 clearBtn.addEventListener("click", clearScores);
