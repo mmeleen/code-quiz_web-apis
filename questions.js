ef98fae203591e908cbb1e64498834892e17a98f
Matt Meleen <mmeleen@utexas.edu>
2020-06-25 19:17:06 -0500

    Rearranged content

--- questions.js
+++ questions.js
@@ -1,20 +1,3 @@
-/*var questionArrayTest = [
-	{
-		title: "This is the first question...?",
-		choices: ["1 YES", "2 no", "3 no"],
-		answer: "1 YES"
-	},
-	{
-		title: "This is the second question...?",
-		choices: ["1 no", "2 YES", "3 no"],
-		answer: "2 YES"
-	},
-	{
-		title: "This is the third question...?",
-		choices: ["1 no", "2 no", "3 YES"],
-		answer: "3 YES"
-	}
-]*/
 
 var questionArray = [	
 	{
@@ -68,3 +51,21 @@ var questionArray = [
 		answer: "D. onclick"
 	}
 ]
+
+/*var questionArrayTest = [
+	{
+		title: "This is the first question...?",
+		choices: ["1 YES", "2 no", "3 no"],
+		answer: "1 YES"
+	},
+	{
+		title: "This is the second question...?",
+		choices: ["1 no", "2 YES", "3 no"],
+		answer: "2 YES"
+	},
+	{
+		title: "This is the third question...?",
+		choices: ["1 no", "2 no", "3 YES"],
+		answer: "3 YES"
+	}
+]*/
